98724ea18e16ae15280af8307db8cc7a87898115
Puentebravo <puentematos@gmail.com>
2021-01-18 23:00:13 -0500

    Added function to render license content if information is provided

--- Utilities/generate.js
+++ Utilities/generate.js
@@ -1,17 +1,21 @@
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    // return `To read the license in its entirety, click here: [License](./LICENSE)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-//     if (license !== null) {
-//         `Content in this project is governed under the ${data.license} license, outlined in the included LICENSE document. `
-//     }
+    if (license !== null) {
+      return `Content in this project is governed under the ${data.license} license, outlined in the included LICENSE document. `
+    } 
 }
 
 // TODO: Create a function to generate markdown for README
@@ -53,7 +57,7 @@ function generateMarkdown(data) {
       
   ## License
       
-  Content in this project is governed under the ${data.license} license, outlined in the included LICENSE document. 
+ ${renderLicenseSection()}
   
   -----------
   
