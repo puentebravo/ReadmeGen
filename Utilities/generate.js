6246c80919aa71ef19d44a0e0ac1f829e34e26c5
Puentebravo <puentematos@gmail.com>
2021-01-18 19:52:38 -0500

    added Utils folder, moved content into function to keep script file clean

---
+++ Utilities/generate.js
@@ -0,0 +1,62 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+    
+  # ${data.title}
+  
+  ## Description 
+      
+  ${data.description}
+      
+      
+  ## Table of Contents
+      
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+      
+      
+  ## Installation
+      
+  ${data.install}
+      
+  ## Usage 
+      
+  ${data.usage}
+      
+  [demo](${data.screenshot})
+      
+      
+  ## Credits
+      
+  ${data.credits}
+      
+      
+  ## License
+      
+  Content in this project is governed under the ${data.license} license, outlined in the included LICENSE document. 
+  
+  -----------
+  
+  Copyright [${data.copyyear}] [${data.copyowner}]`;
+}
+
+module.exports = {
+    generateMarkdown,
+    renderLicenseBadge,
+    renderLicenseLink,
+    renderLicenseSection
+}
