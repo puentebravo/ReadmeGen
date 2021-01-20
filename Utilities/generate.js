ae10bb91e99a6f6c3a986bd753193778cc5162f6
Puentebravo <puentematos@gmail.com>
2021-01-20 00:15:01 -0500

    Fixed functions so that license information correctly renders

--- Utilities/generate.js
+++ Utilities/generate.js
@@ -1,7 +1,4 @@
 
-
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if (license !== "none") {
         return `![](https://img.shields.io/badge/license-${license}-blue)`
@@ -9,43 +6,42 @@ function renderLicenseBadge(license) {
     return ``
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
-    if (license !== null) {
-    return `To read the license in its entirety, click here: [License](./LICENSE)`
+    if (license !== "none") {
+    return `To read the license in its entirety, click here: [${license}](./LICENSE)`
     } else {
         return ``
     }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
-    if (license !== null) {
-      return `Content in this project is governed under the following license:`
+    if (license !== "none") {
+      return `Content in this project is governed under the ${license} License.`
     } else {
         return ``
     }
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `
     
-  # ${data.title} ${renderLicenseBadge()}
+  # ${data.title} ${renderLicenseBadge(data.license)}
   
   ## Description 
       
   ${data.description}
       
-      
   ## Table of Contents
       
   * [Installation](#installation)
   * [Usage](#usage)
-  * [Features](#features)
   * [Credits](#credits)
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  * [Tests](#tests)
   * [License](#license)
       
       
@@ -58,20 +54,27 @@ function generateMarkdown(data) {
   ${data.usage}
       
   [demo](${data.screenshot})
-      
-  ## Features  
 
   ## Credits
       
   ${data.credits}
       
-      
+  ## Contributing
+
+  ## Questions?
+
+
+  
+  ## Tests
+
+
+
   ## License
       
-  ${renderLicenseSection()} ${data.license} 
+  ${renderLicenseSection(data.license)} 
 
-  ${renderLicenseLink()}
-  
+  ${renderLicenseLink(data.license)}
+
   -----------
   
   Copyright [${data.copyyear}] [${data.copyowner}]`;
